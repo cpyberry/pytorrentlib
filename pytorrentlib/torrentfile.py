5d4d7fecc884fcfe5f652b4494fddac566254094
cpyberry <cpyberry222@gmail.com>
2021-04-05 15:04:00 +0900

    feat: create a class for parsing torrent file
    add path argument to initializer

---
+++ pytorrentlib/torrentfile.py
@@ -0,0 +1,14 @@
+import hashlib
+import pybencode
+
+
+class ParseTorrentFile:
+	"""Class for parsing torrent file
+	"""
+
+	def __init__(self, path: str):
+		"""
+		Args:
+			path (str): path of torrent file you want to read
+		"""
+		pass
