c21a6e682a34388cc4612ab86097a08b7dce3f4c
cpyberry <cpyberry222@gmail.com>
2021-04-12 21:35:26 +0900

    feat: add a function operation_key
    The key part is unconditionally decoded

--- pytorrentlib/torrentfile.py
+++ pytorrentlib/torrentfile.py
@@ -1,5 +1,18 @@
 import hashlib
 import pybencode
+import pytransform
+
+
+ENCODING = "ascii"
+
+
+class DecodeTorrentFile:
+	"""After decoding in bencode format, decode the binary part that meets the conditions
+	"""
+
+	@staticmethod
+	def operation_key(key: bytes, origin: dict, value: bytes) -> str:
+		return key.decode(ENCODING)
 
 
 class ParseTorrentFile:
