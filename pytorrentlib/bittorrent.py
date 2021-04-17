16012d5d52bd6f4a76d415a23e339e321b67f852
cpyberry <cpyberry222@gmail.com>
2021-04-17 15:15:11 +0900

    feat: add a function handshake
    Create handshake message

--- pytorrentlib/bittorrent.py
+++ pytorrentlib/bittorrent.py
@@ -20,3 +20,21 @@ class CreateMessage:
 		"""
 		length = len(data) + 1  # add 1 byte of message_id
 		return struct.pack(">IB", length, message_id.value) + data
+
+	@staticmethod
+	def handshake(info_hash: bytes, peer_id: bytes) -> bytes:
+		"""Create handshake message
+
+		Args:
+			info_hash (bytes): sha1 hash of torrentfile info dictionary
+			peer_id (bytes): unique id of the client generated at startup
+
+		Returns:
+			bytes: handshake message
+		"""
+		encoding = "ascii"
+		ptsr = "BitTorrent protocol".encode(encoding)
+		ptstlen = struct.pack(">B", len(ptsr))
+		empty = bytes(8)
+		handshake = ptstlen + ptsr + empty + info_hash + peer_id
+		return handshake
