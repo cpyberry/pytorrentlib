afa3738e2921af63d53e8388554e71d8f99ffb26
cpyberry <cpyberry222@gmail.com>
2021-08-16 00:57:58 +0900

    feat: add a function read to Block class
    Read the specified piece.

--- pytorrentlib/block.py
+++ pytorrentlib/block.py
@@ -17,6 +17,29 @@ class Block:
 		self.piece_length = len(piece_hash_list)
 		self.piece_status_list = [False] * self.piece_length
 
+	def read(self, index: int, offset=0, size=-1) -> bytes:
+		"""Read the specified piece.
+
+		If you omit the size argument, all the data from the offer argument is read.
+
+		Args:
+			index (int): piece index.
+			offset (int, optional): start position to read. Defaults to 0.
+			size (int, optional): size to read. Defaults to -1.
+
+		Returns:
+			bytes: read piece data.
+		"""
+		path = self.get_piece_path(index)
+
+		if not os.path.exists(path):
+			return b""
+
+		with open(path, "rb") as f:
+			f.seek(offset)
+			data = f.read(size)
+		return data
+
 	def get_piece_path(self, index: int) -> str:
 		"""Get the path that stores the piece.
 
