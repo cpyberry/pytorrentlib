ddc04dc14e89df0333a29996e3d95ca2e878dd36
cpyberry <cpyberry222@gmail.com>
2021-08-16 00:31:02 +0900

    feat: add a Block class constructor

--- pytorrentlib/block.py
+++ pytorrentlib/block.py
@@ -2,4 +2,14 @@ class Block:
 	"""
 	This class manages torrent pieces.
 	"""
-	pass
+
+	def __init__(self, piece_hash_list: list, piece_dir_path="."):
+		"""
+		Args:
+			piece_hash_list (int): the pieces in the torrentfile.
+			piece_dir_path (str, optional): directory path to save downloaded pieces. Defaults to ".".
+		"""
+		self.piece_hash_list = piece_hash_list
+		self.piece_dir_path = piece_dir_path
+		self.piece_length = len(piece_hash_list)
+		self.piece_status_list = [False] * self.piece_length
