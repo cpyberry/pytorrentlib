0c2c3b4a97e44d2c8a90c082acfa54e8ae363682
cpyberry <cpyberry222@gmail.com>
2021-08-16 18:55:22 +0900

    feat: add a function get_index to ParsePiece class

--- pytorrentlib/bittorrent.py
+++ pytorrentlib/bittorrent.py
@@ -282,3 +282,14 @@ class ParseRequest(BaseParse):
 class ParsePiece(BaseParse):
 	def __init__(self, message: bytes):
 		super().__init__(message)
+
+	def get_index(self) -> int:
+		"""get the index
+
+		This is zero-based piece index.
+
+		Returns:
+			int: the index
+		"""
+		index = struct.unpack_from(">I", self.content)[0]
+		return index
