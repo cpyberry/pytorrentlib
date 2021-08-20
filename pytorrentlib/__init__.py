3205ce6f0bc28aa313c497e007ffec677c1cbcc4
cpyberry <cpyberry222@gmail.com>
2021-08-21 00:17:08 +0900

    feat: update __version__ in __init__.py
    Update version to v1.1.1

--- pytorrentlib/__init__.py
+++ pytorrentlib/__init__.py
@@ -29,6 +29,6 @@ from .tracker import EventStatus, Tracker
 __copyright__ = "Copyright 2021 cpyberry"
 __url__ = "https://github.com/cpyberry/pytorrentlib"
 __license__ = "Apache-2.0 License"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "cpyberry"
 __author_email__ = "cpyberry222@gmail.com"
