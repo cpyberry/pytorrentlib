851789bf59343a5ab2899d5e56de95ce73fa0fe4
cpyberry <cpyberry222@gmail.com>
2021-09-14 21:21:53 +0900

    refactor: move information in __init__.py to __info__.py
    To make it easier to load with setup.py

---
+++ pytorrentlib/__info__.py
@@ -0,0 +1,19 @@
+"""
+Copyright 2021 cpyberry
+https://github.com/cpyberry/pytorrentlib
+
+cpyberry
+email: cpyberry222@gmail.com
+github: https://github.com/cpyberry
+"""
+
+
+__title__ = "pytorrentlib"
+__description__ = "Simple torrent library"
+__copyright__ = "Copyright 2021 cpyberry"
+__url__ = "https://github.com/cpyberry/pytorrentlib"
+__license__ = "Apache-2.0 License"
+__version__ = "1.1.1"
+__author__ = "cpyberry"
+__author_email__ = "cpyberry222@gmail.com"
+__python_requires__ = ">=3.6"
--- pytorrentlib/__init__.py
+++ pytorrentlib/__init__.py
@@ -35,13 +35,14 @@ from .messageid import MessageIdFlag, MessageIdEnum
 from .torrentfile import ENCODING, ParseTorrentFile
 from .tracker import EventStatus, Tracker
 
-
-__title__ = "pytorrentlib"
-__description__ = "Simple torrent library"
-__copyright__ = "Copyright 2021 cpyberry"
-__url__ = "https://github.com/cpyberry/pytorrentlib"
-__license__ = "Apache-2.0 License"
-__version__ = "1.1.1"
-__author__ = "cpyberry"
-__author_email__ = "cpyberry222@gmail.com"
-__python_requires__ = ">=3.6"
+from .__info__ import (
+	__title__,
+	__description__,
+	__copyright__,
+	__url__,
+	__license__,
+	__version__,
+	__author__,
+	__author_email__,
+	__python_requires__
+)
